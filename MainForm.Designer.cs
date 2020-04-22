dd7589e75d90bb388c27211fac24c28ba0a96eef
WuRuidongReyes <963211782@qq.com>
2020-04-22 17:30:30 +0800

    添加项目文件。

---
+++ MainForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace BookShopManagement
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel_DashBoard = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Label_Title = new System.Windows.Forms.Label();
+            this.PictureBox_Logo = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Button_Minimize = new System.Windows.Forms.Button();
+            this.Button_Close = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.Button_Home = new System.Windows.Forms.Button();
+            this.panel_DashBoard.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Logo)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_DashBoard
+            // 
+            this.panel_DashBoard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+            this.panel_DashBoard.Controls.Add(this.Button_Home);
+            this.panel_DashBoard.Controls.Add(this.panel1);
+            this.panel_DashBoard.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel_DashBoard.Location = new System.Drawing.Point(0, 0);
+            this.panel_DashBoard.Name = "panel_DashBoard";
+            this.panel_DashBoard.Size = new System.Drawing.Size(215, 720);
+            this.panel_DashBoard.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Label_Title);
+            this.panel1.Controls.Add(this.PictureBox_Logo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(215, 150);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(28, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kangbashen Erdos";
+            // 
+            // Label_Title
+            // 
+            this.Label_Title.AutoSize = true;
+            this.Label_Title.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_Title.ForeColor = System.Drawing.Color.White;
+            this.Label_Title.Location = new System.Drawing.Point(30, 74);
+            this.Label_Title.Name = "Label_Title";
+            this.Label_Title.Size = new System.Drawing.Size(154, 22);
+            this.Label_Title.TabIndex = 1;
+            this.Label_Title.Text = "North Book Shop";
+            // 
+            // PictureBox_Logo
+            // 
+            this.PictureBox_Logo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.PictureBox_Logo.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox_Logo.Image")));
+            this.PictureBox_Logo.Location = new System.Drawing.Point(82, 21);
+            this.PictureBox_Logo.Name = "PictureBox_Logo";
+            this.PictureBox_Logo.Size = new System.Drawing.Size(50, 50);
+            this.PictureBox_Logo.TabIndex = 0;
+            this.PictureBox_Logo.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.Button_Minimize);
+            this.panel2.Controls.Add(this.Button_Close);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(215, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(985, 50);
+            this.panel2.TabIndex = 1;
+            // 
+            // Button_Minimize
+            // 
+            this.Button_Minimize.FlatAppearance.BorderSize = 0;
+            this.Button_Minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Minimize.Image = ((System.Drawing.Image)(resources.GetObject("Button_Minimize.Image")));
+            this.Button_Minimize.Location = new System.Drawing.Point(885, 0);
+            this.Button_Minimize.Name = "Button_Minimize";
+            this.Button_Minimize.Size = new System.Drawing.Size(50, 50);
+            this.Button_Minimize.TabIndex = 0;
+            this.Button_Minimize.UseVisualStyleBackColor = true;
+            // 
+            // Button_Close
+            // 
+            this.Button_Close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button_Close.FlatAppearance.BorderSize = 0;
+            this.Button_Close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.Button_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Close.Image = ((System.Drawing.Image)(resources.GetObject("Button_Close.Image")));
+            this.Button_Close.Location = new System.Drawing.Point(935, 0);
+            this.Button_Close.Name = "Button_Close";
+            this.Button_Close.Size = new System.Drawing.Size(50, 50);
+            this.Button_Close.TabIndex = 0;
+            this.Button_Close.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(215, 50);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(985, 100);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(215, 150);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(985, 570);
+            this.panel4.TabIndex = 3;
+            // 
+            // Button_Home
+            // 
+            this.Button_Home.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Button_Home.FlatAppearance.BorderSize = 0;
+            this.Button_Home.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Home.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Button_Home.ForeColor = System.Drawing.Color.White;
+            this.Button_Home.Location = new System.Drawing.Point(0, 150);
+            this.Button_Home.Margin = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.Button_Home.Name = "Button_Home";
+            this.Button_Home.Size = new System.Drawing.Size(215, 50);
+            this.Button_Home.TabIndex = 1;
+            this.Button_Home.Text = "button2";
+            this.Button_Home.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1200, 720);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel_DashBoard);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel_DashBoard.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Logo)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_DashBoard;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Label_Title;
+        private System.Windows.Forms.PictureBox PictureBox_Logo;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button Button_Minimize;
+        private System.Windows.Forms.Button Button_Close;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button Button_Home;
+    }
+}
+
